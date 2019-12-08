cb190ff04f93ca52238b2835c3c59a1912cc1779
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-08 15:06:09 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,18 @@ require 'pry'
 
 def remove_strawberry(contacts)
 
+  contacts = {
+    "Jon Snow" => {
+      name: "Jon",
+      email: "jon_snow@thewall.we",
+      favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+    },
+    "Freddy Mercury" => {
+      name: "Freddy",
+      email: "freddy@mercury.com",
+      favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+    }
+  }
   contacts.each do |person, contact_details_hash|
   binding.pry
   end
